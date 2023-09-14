6d26e1796ebdece04b52311763b861dd3d06e6dd
JaeHyeokSeong <seong5763@gmail.com>
2023-09-14 21:09:59 +1000

    main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("hello I'm Jae!!");
         System.out.println("how are you today?");
         System.out.println("I will finish it!!");
+        System.out.println("I can do it!!");
     }
 }
