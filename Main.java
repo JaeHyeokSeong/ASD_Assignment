d3c17551786ed32238c735f4c48898d289112dd9
Pascal Terwesten <pascal@Pascals-MacBook-Air-3.local>
2023-09-14 20:41:23 +1000

    update main.java in pascal_feature branch

--- Main.java
+++ Main.java
@@ -2,9 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("TEST");
     }
-
-    System.out.println("TEST!");
-    System.out.println("TEST!");
-
 }
